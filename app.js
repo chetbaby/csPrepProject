0b95e3a8827d6fbc52986edaa8dc570324b97397
chetbaby <cbaby@CBMB.local>
2019-02-18 10:09:12 -0800

    took out comments

--- app.js
+++ app.js
@@ -10,20 +10,11 @@ var params = {
   lang: "en"
 };
 
-//console.log();
-
 T.get("search/tweets", params, function(err, data, response) {
-  // If there is no error, proceed
   if (!err) {
-    // Loop through the returned tweets
     for (let i = 0; i < data.statuses.length; i++) {
-      // Get the tweet Id from the returned data
       let id = data.statuses[i].id_str,
         user = data.statuses[i].user.screen_name;
-      // Try to Favorite the selected Tweet
-      // client.post('statuses/update', {in_reply_to_status_id: id_str,
-      // status: '@' + screen_name + ' I think you mean "blockchain"'},
-
       T.post(
         "statuses/update",
         {
@@ -31,12 +22,9 @@ T.get("search/tweets", params, function(err, data, response) {
           status: "@" + user + "🤖"
         },
         function(err, response) {
-          // If the favorite fails, log the error message
           if (err) {
             console.log(err[0].message);
-          }
-          // If the favorite is successful, log the url of the tweet
-          else {
+          } else {
             let username = response.user.screen_name,
               tweetId = response.id_str;
             console.log(`Replied to: ${username}`);
@@ -45,6 +33,7 @@ T.get("search/tweets", params, function(err, data, response) {
       );
       // console.log(id, user);
     }
+    //TODO: put other post requests here
   } else {
     console.log(err);
   }
