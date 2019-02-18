65ebfee8ccb8ac7b955c12d7dff96e0043c4d1f3
chetbaby <cbaby@CBMB.local>
2019-02-18 11:58:28 -0800

    replies and follows

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var config = require("./config.sys");
 var T = new Twitter(config);
 
 var params = {
-  q: "love OR hate",
+  q: "#venicebeach",
   count: 2,
   result_type: "mixed",
   lang: "en"
@@ -13,8 +13,9 @@ var params = {
 T.get("search/tweets", params, function(err, data, response) {
   if (!err) {
     for (let i = 0; i < data.statuses.length; i++) {
-      let id = data.statuses[i].id_str,
-        user = data.statuses[i].user.screen_name;
+      let id = data.statuses[i].id_str;
+      let user = data.statuses[i].user.screen_name;
+      let screen_name = data.statuses[i].user.screen_name;
       T.post(
         "statuses/update",
         {
@@ -25,13 +26,21 @@ T.get("search/tweets", params, function(err, data, response) {
           if (err) {
             console.log(err[0].message);
           } else {
-            let username = response.user.screen_name,
-              tweetId = response.id_str;
-            console.log(`Replied to: ${username}`);
+            // let username = response.user.screen_name,
+            tweetId = response.id_str;
+            console.log(`Replied to: ${screen_name}`);
           }
         }
       );
       // console.log(id, user);
+      T.post("friendships/create", { screen_name }, function(err, res) {
+        // if there was an error, we could not follow the user, console.log err
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`Followed ${screen_name}!`);
+        }
+      });
     }
     //TODO: put other post requests here
   } else {
